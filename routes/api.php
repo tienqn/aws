dd47dbaea4f67fa6beb0de5bef4c99343510a705
tienpham <tiens8acc@enjoyworks.co.kr>
2020-10-22 17:04:56 +0700

    add api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('aws', function(){
+	return response()->json([
+		'status' => 'OK',
+		'data' => 'Hi! i am Tien'
+	]);
+});
